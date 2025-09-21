daf6c1212fa678ebb6db4684f2f84381048e0dfc
bichphuong-jun <phuonghoanhsona1@gmail.com>
2025-09-21 12:07:14 +0700

    Thêm thư mục HW1

---
+++ HW1/Introduction/src/Introduction.java
@@ -0,0 +1,12 @@
+public class Introduction {
+    public static void main (String[] agrs) {
+        System.out.println("Bùi Thị Bích Phượng\t24021601\tLớp 2526I_INT2210_11\t@phuong060112\t24021601@vnu.edu.vn");
+        for (int i = 9; i >= 2; i--) {
+            System.out.println(i + " bottles of beer on the wall, " + i + "bottles of beer");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("1 bottle of beer on the wall, 1 bottle of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
