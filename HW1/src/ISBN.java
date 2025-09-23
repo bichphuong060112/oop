c84c01dad571ebd99f530d1255ecf50f1467d124
bichphuong-jun <phuonghoanhsona1@gmail.com>
2025-09-23 11:25:15 +0700

    HW1 phần 1.3

---
+++ HW1/src/ISBN.java
@@ -0,0 +1,18 @@
+public class ISBN {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int sum = 0;
+        for (int i = 2; i <= 10; i++) {
+            int digit = n % 10;
+            sum = sum + i * digit;
+            n /= 10;
+        }
+        System.out.print(args[0]);
+        if (sum % 11 == 1) {
+            System.out.println("X");
+        }
+        else {
+            System.out.println(11 - (sum % 11));
+        }
+    }
+}
